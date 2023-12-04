4e3aafe7e9ca4095a9e7d199a98097a543cc840a
Guillherme Alves Vaz <76267743+guiavaz@users.noreply.github.com>
2023-12-04 01:02:26 -0300

    Update cafeteria.c

--- cafeteria/cafeteria.c
+++ cafeteria/cafeteria.c
@@ -1,23 +1,70 @@
-#include<stdio.h>
-#include<stdlib.h>
 #include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "cafeteria.h"
 
-void cadastraProduto()
-{
-    //Não implementado
-}
+int main() {
+  setlocale(LC_ALL, "Portuguese");
 
-void consultaProduto()
-{
-    //Não implementado
-}
+  printf("-------Cafeteria dos Guris-------\n");
 
-void excluiProduto()
-{
-    //Não implementado
-}
+  int option = 0;
 
-void criaOrcamento()
-{
-    //Não implementado
+  do {
+
+    printf("\nSelecione uma opcao:\n\n");
+    printf("1- Realizar Venda\n");
+    printf("2- Consultar Produto\n");
+    printf("3- Cadastrar Produto\n");
+    printf("4- Excluir Produto\n");
+    printf("5- Saidas\n");
+    printf("6- Relatorio financeiro\n");
+    printf("99- Sair\n\n");
+
+    scanf("%i", &option);
+
+    switch (option) {
+
+    case 1:
+      realizarVenda();
+      getchar();
+      break;
+
+    case 2:
+      consultaProduto();
+      getchar();
+      break;
+
+    case 3:
+      cadastraProduto();
+      getchar();
+      break;
+
+    case 4:
+      excluiProduto();
+      getchar();
+      break;
+
+    case 5:
+      saidas();
+      getchar();
+      break;
+
+    case 6:
+      relatorioFinanceiro();
+      getchar();
+      break;
+
+    case 99:
+      printf("\nPrograma Encerrado!\n");
+      break;
+
+    default:
+      printf("Opcao invalida. Tente novamente.\n");
+    }
+  } while (option != 99);
+
+  return 0;
 }
